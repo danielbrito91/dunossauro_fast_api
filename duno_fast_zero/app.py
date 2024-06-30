509a455b3fe2fe256053f54d60dc9692e6e43517
danielbrito91 <danieloliveiradebrito@gmail.com>
2024-06-30 16:06:26 -0300

    aula 02 - exercícios

--- duno_fast_zero/app.py
+++ duno_fast_zero/app.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from duno_fast_zero.schemas import Message
+from http import HTTPStatus
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_class=HTMLResponse)
 def read_root():
-    return {'message': 'Olá, galera!'}
+    html_content = """<html>
+        <head>
+            <title>FastAPI</title>
+        </head>
+        <body>
+            <h1>olá mundo!</h1>
+        </body>
+    </html>
+            """
+    return html_content
--- tests/test_app.py
+++ tests/test_app.py
@@ -10,3 +10,11 @@ def test_read_root_must_return_ok_and_hello_world():
     response = client.get('/')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá, galera!'}
+
+def test_read_root_must_return_ok_and_ola_mundo_html():
+    client = TestClient(app)
+    response = client.get('/')
+    
+    assert response.status_code == HTTPStatus.OK
+    assert "<html>" in response.text
+    assert "olá mundo" in response.text
